d4e8ca59ea94dea7617a61b1bd60be7f2788dcec
soboli89 <84281763+soboli89@users.noreply.github.com>
2025-01-28 17:27:13 -0600

    changed to function component

--- src/GuessControl.jsx
+++ src/GuessControl.jsx
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
 import Button from "./Button";
+import { useState } from "react";
 
-class GuessControl extends Component {
+
+function GuessControl ({onGuess}) {
+
+  const [currentGuess, setCurrentGuess] = useState('');
+  const handleInputChange = (e) => {
+    setCurrentGuess(e.target.value);
+  }
+  const onSubmitGuess = () => {
+    onGuess (Number(currentGuess));
+    setCurrentGuess('');
+
+  }
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={currentGuess}
+        onChange={handleInputChange}
+      />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  );
+}
+
+class GuessControlOld extends Component {
   constructor(props) {
     super(props);
 
